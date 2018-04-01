9532d48a3f8db22da3e50df9d1525813c857c88c
Paul Schlueter <paul@paulschlueter.com>
2018-04-01 15:08:26 -0500

    Basic Plugin Working with No Functionality

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Orthodox Daily Readings
+Plugin URI: http://paulschlueter.com
+Description: This plugin allows you to post the current day's readings and fasting rule from antiochian.org on your own website.
+Author: Paul Schlueter
+Version: 1.0.0
+Author URI: http://paulschlueter.com
+*/
+
+function odr_links() {
+	echo "<div>These are the links.</div>";
+}
+
+add_shortcode('daily_readings_links', 'odr_links');
+
+function odr_page() {
+	echo "<div>This is the page.</div>";
+}
+
+add_shortcode('daily_readings_page', 'odr_page');
+
+?>
