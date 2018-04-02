bbf1a3804292e3a9149b2b877e515caad83967e5
Paul Schlueter <paul@paulschlueter.com>
2018-04-01 23:06:07 -0500

    Added Activation/Deactivation Handlers

--- orthodox-daily-readings.php
+++ orthodox-daily-readings.php
@@ -10,16 +10,85 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Author URI: http://paulschlueter.com
 */
 
-function odr_links() {
-	echo "<div>These are the links.</div>";
+/**
+ * Represents a single reading
+ */
+class ODR_Reading {
+	private $title;
+	private $text;
+
+	public function __construct(string $title, string $text) {
+		$this->title = $title;
+		$this->text = $text;
+	}
+
+	public function get_text() {
+		return $this->text;
+	}
+
+	public function get_title() {
+		return $this->title;
+	}
 }
 
-add_shortcode('daily_readings_links', 'odr_links');
+/**
+ * Class to hold all of the readings data
+ */
+class ODR_ReadingsData {
+	private $date;
+	private $readings;
+	private $fastingText;
 
-function odr_page() {
-	echo "<div>This is the page.</div>";
+	/**
+	 * Constructor
+	 * @param string $date the date for these readings
+	 * @param array $readings an array of ODR_Reading objects (the actual readings)
+	 * @param string $fastingText the text with the fasting rule for the day
+	 * @return ODR_ReadingsData
+	 */
+	public function __construct(string $date, array $readings, string $fastingText) {
+		$this->date = $date;
+		$this->readings = $readings;
+		$this->fastingText = $fastingText;
+	}
 }
 
-add_shortcode('daily_readings_page', 'odr_page');
+/**
+ * Handles activation and deactivation of the plugin
+ */
+class ODR_ActivationHandler {
 
+	/**
+	 * Constructor
+	 * @return ODR_ActivationHandler
+	 */
+	public function __construct() {
+		register_activation_hook(__FILE__, 'ODR_ActivationHandler::on_activate');
+		register_deactivation_hook(__FILE__, 'ODR_ActivationHandler::on_deactivate');
+	}
+
+	public static function on_activate() {
+		// Add options to the database if they don't already exist
+		update_option('odr_readings', 0);
+	}
+
+	public static function on_deactivate() {
+		// Do nothing
+	}
+}
+
+// function odr_links() {
+// 	echo "<div>These are the links.</div>";
+// }
+
+// add_shortcode('daily_readings_links', 'odr_links');
+
+// function odr_page() {
+// 	echo "<div>This is the page.</div>";
+// }
+
+// add_shortcode('daily_readings_page', 'odr_page');
+
+// Instantiate the activation/deactivation handler
+$activator = new ODR_ActivationHandler();
 ?>
