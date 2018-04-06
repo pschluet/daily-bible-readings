2140108ccd1a30c3099aed3c52a438cad6ff4635
Paul Schlueter <paul@paulschlueter.com>
2018-04-06 09:58:21 -0500

    Added Uninstall Code, Resolved #2

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,11 @@
 <?php
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
+	die;
 }
 
+include_once 'orthodox-daily-readings.php';
+
+// Remove the plugin's data from the Wordpress options table
+delete_option(ODR_LocalDataStoreInterface::DATA_KEY);
 ?>
