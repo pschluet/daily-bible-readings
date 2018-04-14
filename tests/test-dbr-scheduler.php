af99ced2bd89404f0452bbe8838e03ff09b92a2f
Paul Schlueter <paul@paulschlueter.com>
2018-04-14 16:08:59 -0500

    Completed Scheduler Tests

--- tests/test-dbr-scheduler.php
+++ tests/test-dbr-scheduler.php
@@ -6,8 +6,11 @@
  */
 class DBR_SchedulerTest extends WP_UnitTestCase {
 
+	private $scheduler;
+
 	public function setUp() {
 		parent::setUp();
+		$this->scheduler = new DBR_Scheduler();
 	}
 
 	public function tearDown() {
@@ -15,14 +18,43 @@ class DBR_SchedulerTest extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test to make sure registration of cron callbacks adds WordPress
+	 * action hooks.
+	 */
+	public function test_that_cron_registration_adds_wp_actions() {
+		$callback = array($this, 'sample_function');
+		$this->scheduler->register_data_sync_callback($callback);
+
+		$this->assertTrue(has_action(DBR_Scheduler::CRON_NAME, $callback) != False && has_action(DBR_Scheduler::SINGLE_EVENT_NAME, $callback) != False);
+	}
+
+	/**
+	 * Test to make sure the recurring cron job is unscheduledscheduled
+	 */
+	public function test_that_recurring_cron_is_unscheduled() {
+		$this->assert_that_recurring_cron_is_scheduled();
+		$this->scheduler->unschedule_data_sync();
+
+		$cronNames = $this->get_scheduled_cron_names();
+		$this->assertNotContains(DBR_Scheduler::CRON_NAME, $cronNames);
+	}
+
+	/**
 	 * Test to make sure the recurring cron job is scheduled
 	 */
-	public function test_that_recurring_cron_is_scheduled() {
-		$scheduler = new DBR_Scheduler();
-		$scheduler->schedule_recurring_data_sync();
+	public function assert_that_recurring_cron_is_scheduled() {
+		$this->scheduler->schedule_recurring_data_sync();
 
 		$cronNames = $this->get_scheduled_cron_names();
 		$this->assertContains(DBR_Scheduler::CRON_NAME, $cronNames);
+	}
+
+	/**
+	 * Placeholder method for passing to the following test:
+	 * test_that_cron_registration_adds_wp_actions
+	 */
+	public function sample_function() {
+		// Does nothing
 	}
 
 	/**
